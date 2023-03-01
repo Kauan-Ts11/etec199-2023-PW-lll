e7fa85a814ccbd5735bc1bb361aacf627b97539a
Kauan Gabriel <113554740+Kauan-Ts11@users.noreply.github.com>
2023-03-01 16:17:55 -0300

    Update listaAlunos.js

--- 02-14/assents/js/listaAlunos.js
+++ 02-14/assents/js/listaAlunos.js
@@ -10,7 +10,15 @@ export const Lista_Alunos = [
     new Aluno ("Henry", 17, 1.24, 15),
     new Aluno ("Miguel", 17, 1.59, 58),
     new Aluno ("Kaio", 16, 1.78, 80),
-    new Aluno ("Kaique", 17, 1.80, 80)
-    
+    new Aluno ("Kaique", 17, 1.80, 80),
+    new Aluno ("Bruna" , 17, 1.68, 67),
+    new Aluno ("Donadoni", 23, 1.75, 80),
+    new Aluno ("Everson", 25, 1.76, 79),
+    new Aluno ("Aline", 24, 1.69, 70),
+    new Aluno ("Thay", 26, 1.50, 60),
+    new Aluno ("Marcos", 30, 1.67, 76),
+    new Aluno ("Rodrigo", 31, 1.70, 73),
+    new Aluno ("Pedro", 14, 1.54, 50),
+    new Aluno ("Ryan", 14, 1.66, 60)
 
 ]
