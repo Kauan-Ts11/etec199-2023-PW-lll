c37378f216880e7d10cca088cd336797c4fabc07
Kauan Gabriel <113554740+Kauan-Ts11@users.noreply.github.com>
2023-03-07 09:11:23 -0300

    Update main.js

--- 02-14/assents/js/main.js
+++ 02-14/assents/js/main.js
@@ -6,11 +6,11 @@ function escreveObj() {
     for(let i = 0; i < Lista_Alunos.length; i++) {
 
         var {nome, imc, categoriaImc} = Lista_Alunos[i]
-        console.log(nome+" -> Imc: "+imc+" -> Categoria: "+categoriaImc)
+        console.log(nome+"\n -> Imc: "+imc+"\n -> Categoria: "+categoriaImc)
 
     }
 
-    console.log(Categorias)
+    console.log("\nContagem das Categorias \n\n" + "Abaixo do Peso: " + Categorias.abaixoDoPeso + "\nPeso Normal: " + Categorias.pesoNormal + "\nSobrepeso: " + Categorias.sobrepeso + "\nObesidade Grau I: " + Categorias.obesidade1 + "\nObesidade Grau II: " + Categorias.obesidade2 + "\nObesidade Grau III: " + Categorias.obesidade3)
 
 }
 
